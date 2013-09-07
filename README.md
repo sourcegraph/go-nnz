ef015c45fbad876d8bac3c366c643ef39aa244e0
Quinn Slack <qslack@qslack.com>
2013-09-06 21:42:04 -0700

    nnz.Int and nnz.String

---
+++ README.md
@@ -0,0 +1,7 @@
+go-nnz
+========================================
+
+Package nnz defines variants of primitive types where the zero value represents
+null when (de)serializing with encoding/json and database/sql.
+
+[![Build Status](https://travis-ci.org/sourcegraph/go-nnz.png)](https://travis-ci.org/sourcegraph/go-nnz)
---
+++ nnz/nnz.go
@@ -0,0 +1,113 @@
+// Package nnz defines variants of primitive types where the zero value
+// represents null when (de)serializing with encoding/json and database/sql.
+package nnz
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+)
+
+type Int int
+
+// Scan implements the database/sql/driver.Scanner interface.
+func (i *Int) Scan(v interface{}) error {
+	if v == nil {
+		*i = 0
+		return nil
+	}
+	switch v := v.(type) {
+	case int:
+		*i = Int(v)
+	case int64:
+		*i = Int(v)
+	default:
+		return fmt.Errorf("nnz: scanning %T, got %T", i, v)
+	}
+	return nil
+}
+
+// Value implements the database/sql/driver.Valuer interface.
+func (i Int) Value() (driver.Value, error) {
+	if i == 0 {
+		return nil, nil
+	}
+	return int(i), nil
+}
+
+// MarshalJSON implements the encoding/json.Marshaler interface.
+func (i Int) MarshalJSON() ([]byte, error) {
+	if i == 0 {
+		return json.Marshal(nil)
+	}
+	return json.Marshal(int(i))
+}
+
+// UnmarshalJSON implements the encoding/json.Unmarshaler interface.
+func (i *Int) UnmarshalJSON(data []byte) error {
+	var v interface{}
+	err := json.Unmarshal(data, &v)
+	if err != nil {
+		return err
+	}
+	if v == nil {
+		*i = 0
+	} else if v, ok := v.(float64); ok {
+		*i = Int(v)
+	} else {
+		return fmt.Errorf("nnz: unmarshaling %T, got %T", i, v)
+	}
+	return nil
+}
+
+type String string
+
+// Scan implements the database/sql/driver.Scanner interface.
+func (s *String) Scan(v interface{}) error {
+	if v == nil {
+		*s = ""
+		return nil
+	}
+	switch v := v.(type) {
+	case []byte:
+		*s = String(v)
+	case string:
+		*s = String(v)
+	default:
+		return fmt.Errorf("nnz: scanning %T, got %T", s, v)
+	}
+	return nil
+}
+
+// Value implements the database/sql/driver.Valuer interface.
+func (s String) Value() (driver.Value, error) {
+	if s == "" {
+		return nil, nil
+	}
+	return string(s), nil
+}
+
+// MarshalJSON implements the encoding/json.Marshaler interface.
+func (s String) MarshalJSON() ([]byte, error) {
+	if s == "" {
+		return json.Marshal(nil)
+	}
+	return json.Marshal(string(s))
+}
+
+// UnmarshalJSON implements the encoding/json.Unmarshaler interface.
+func (s *String) UnmarshalJSON(data []byte) error {
+	var v interface{}
+	err := json.Unmarshal(data, &v)
+	if err != nil {
+		return err
+	}
+	if v == nil {
+		*s = ""
+	} else if v, ok := v.(string); ok {
+		*s = String(v)
+	} else {
+		return fmt.Errorf("nnz: unmarshaling %T, got %T", s, v)
+	}
+	return nil
+}
